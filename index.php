83056f14fed8c9c1f178a1ccc058238fb7b6bf6b
Edgleidson <wake-up88@hotmail.com>
2022-03-10 16:24:34 -0300

    Criamos a classe Page

--- index.php
+++ index.php
@@ -2,18 +2,20 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim; //Usando o namespace Slim.
+use \Hcode\Page;//Usando o namespace Hcode.
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql(); //Instanciando a classe Sql.
+	$page = new Page(); //Ao chamar o new Page() ele vai acionar o __construct() com header.html que tem o cabeçalho do template.
 
-	$resultado = $sql->select("SELECT * FROM tb_users");
+	$page->setTpl("index"); //Adicionar o arquivo index.html que tem o corpo do template.
 
-	echo json_encode($resultado);
-
+	//Quanto terminar a execução vai ser acionado o __destruct() com o footer.html que tem a tag para fechar o html finalizando o template.
 });
 
 $app->run();
---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Hcode;
+
+use Rain\tpl; //Usando o namespace Rain.
+
+class Page{
+
+	private $tpl;
+	private $options =[];
+	private $defaults = [ 'dados'=>[] ];
+
+	//Método mágico para ser executado no início.
+	public function __construct($opts = array()){
+
+		//Mesclar os Arrays.
+		$this->options = array_merge($this->defaults, $opts); // O Array($opts) vai sobre escrever o Array($defaults) na varíavel $options. 
+
+		$config = array(
+			"tpl_dir"       => $_SERVER['DOCUMENT_ROOT'] . "/views/",
+			"cache_dir"     => $_SERVER['DOCUMENT_ROOT'] . "/views-cache/",
+			"debug"         => false
+		);
+
+		Tpl::configure( $config );
+
+		$this->tpl = new Tpl;
+
+		$this->setDados($this->options['dados']);
+
+		$this->tpl->draw("header"); //Desenhar o template na parte do Cabeçalho.
+
+	}//Fim do Construct.
+
+
+	private function setDados($dados = array()){
+
+		foreach ($dados as $key => $value) {
+			$this->tpl->assign($key, $value);
+		}//Fim do Foreach.
+	}
+
+
+	//Conteúdo do corpo.
+	public function setTpl($name, $dados = array(), $returnHTML = false){
+
+		$this->setDados($dados);
+
+		return $this->tpl->draw($name, $returnHTML); //Desenhar o template na parte do corpo.
+	}
+
+
+	//Método mágico para ser executado no final.
+	public function __destruct(){
+		$this->tpl->draw("footer"); //Desenhar o template na parte do Rodapé.
+	}//Fim do Destruct.
+}
+
+
+?>
