fb36623429429d5a8dfa98f00a594f20a9f1ba95
Edgleidson <wake-up88@hotmail.com>
2022-03-17 18:23:38 -0300

    Esqueci a senha --- Com erro*

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $app = new Slim();
 $app->config('debug', true);
 
 
-//Rota inicial -> http://www.hcodecommerce.com.br:81/.
+//Rota inicial -> http://www.hcodecommerce.com.br:81/     (get)
 $app->get('/', function() {
     
 	$page = new Page(); //Ao chamar o new Page() ele vai acionar o __construct() com header.html que tem o cabeçalho do template.
@@ -25,7 +25,7 @@ $app->get('/', function() {
 });//Fim Rota.
 
 
-//Rota -> http://www.hcodecommerce.com.br:81/admin
+//Rota -> http://www.hcodecommerce.com.br:81/admin     (get)
 $app->get('/admin', function() {
 
 	User::verifyLogin(); //Acessando método static.
@@ -38,7 +38,7 @@ $app->get('/admin', function() {
 });//Fim Rota.
 
 
-//Rota -> http://www.hcodecommerce.com.br:81/admin/login
+//Rota -> http://www.hcodecommerce.com.br:81/admin/login     (get)
 $app->get('/admin/login', function(){
 
 	$page = new PageAdmin([
@@ -47,33 +47,30 @@ $app->get('/admin/login', function(){
 	]);
 
 	$page->setTpl("login");
-
 });//Fim da Rota.
 
 
-//Rota POST ->
+//Rota -> http://www.hcodecommerce.com.br:81/admin/login      (post)
 $app->post('/admin/login', function(){
 
 	User::login($_POST['login'], $_POST['password']);
 
 	header("Location: /admin");
 	exit();
-
 });//Fim Rota POST.
 
 
-//Rota
+//Rota -> http://www.hcodecommerce.com.br:81/admin/logout      (get)
 $app->get('/admin/logout', function(){
 
 	User::logout();
 
 	header("Location: /admin/login");
 	exit();
-
 });//Fim da Rota.
 
 
-//Rota
+//Rota -> http://www.hcodecommerce.com.br:81/admin/users      (get)
 $app->get('/admin/users', function(){
 
 	User::verifyLogin();
@@ -88,7 +85,7 @@ $app->get('/admin/users', function(){
 });//Fim Rota.
 
 
-//Rota.
+//Rota -> http://www.hcodecommerce.com.br:81/admin/users/create    "INSERT" (get)
 $app->get('/admin/users/create', function(){
 
 	User::verifyLogin();
@@ -99,7 +96,7 @@ $app->get('/admin/users/create', function(){
 });//Fim Rota.
 
 
-//Rota com parâmetro. "DELETE"
+//Rota -> http://www.hcodecommerce.com.br:81/admin/users       "DELETE" (get)
 $app->get("/admin/users/:iduser/delete", function($iduser) {
 
 	User::verifyLogin();	
@@ -115,7 +112,7 @@ $app->get("/admin/users/:iduser/delete", function($iduser) {
 });
 
 
-//Rota com parâmetro.
+//Rota -> http://www.hcodecommerce.com.br:81/admin/users      "UPDATE" (get)
 $app->get('/admin/users/:iduser', function($iduser){
 
 	User::verifyLogin();
@@ -132,7 +129,7 @@ $app->get('/admin/users/:iduser', function($iduser){
 });//Fim Rota.
 
 
-//Rota POST. "INSERT"
+//Rota -> http://www.hcodecommerce.com.br:81/admin/users/create       "INSERT" (post)
 $app->post('/admin/users/create', function(){
 
 	User::verifyLogin();
@@ -150,7 +147,7 @@ $app->post('/admin/users/create', function(){
 });//Fim Rota.
 
 
-//Rota POST com parâmetro. "UPDATE"
+//Rota com parâmetro -> http://www.hcodecommerce.com.br:81/admin/users/     "UPDATE" (post)
 $app->post('/admin/users/:iduser', function($iduser){
 
 	User::verifyLogin();
@@ -169,6 +166,81 @@ $app->post('/admin/users/:iduser', function($iduser){
 	exit;
 });//Fim Rota POST.
 
-$app->run();
 
+//Rota -> http://www.hcodecommerce.com.br:81/admin/forgot      "Esqueci a Senha"  (get)
+$app->get("/admin/forgot", function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");	
+});//Fim Rota.
+
+
+//Rota -> http://www.hcodecommerce.com.br:81/admin/forgot       "Esqueci a Senha"  (post)
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+});//Fim Rota POST.
+
+
+//Rota -> http://www.hcodecommerce.com.br:81/admin/forgot/sent      "Esqueci a Senha - Envio de Email" (get)
+$app->get("/admin/forgot/sent", function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+});;//Fim Rota.
+
+
+//Rota -> http://www.hcodecommerce.com.br:81/admin/forgot/reset      "Esqueci a Senha - Alterar Senha" (get)
+$app->get("/admin/forgot/reset", function(){
+
+	$user = User::validForgotDecrypt($_GET['code']);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user['desperson'],
+		"code"=>$_GET['code']
+	));
+}); //Fim Rota.
+
+
+//Rota -> http://www.hcodecommerce.com.br:81/admin/forgot/reset      "Esqueci a Senha - Alterar Senha" (post)
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);	
+
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = User::getPasswordHash($_POST["password"]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+
+}); //Fim Rota
+
+$app->run();
  ?>
